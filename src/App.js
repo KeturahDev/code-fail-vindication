e4f9baf79877385186c5fb448fe8e89e32ea0456
Keturah Howard <keturah.dev@gmail.com>
2024-01-31 22:51:58 -0800

    get basic idea of this project down

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>My Vindication</h1>
+
+      <p>
+        I fail some things in code challenges. It makes me really sad sometimes.
+        But lets take those moments, and relive them in an environment where I
+        have the time to do them right.{" "}
+      </p>
+
+      <h3>Things I've failed at in the past</h3>
+      <ul>
+        <li>Taking long numbers like 7500000.75 and converting it to 7.5M</li>
+        <li>Make a viewport responsive css style for a grid</li>
+        <li>Thats all i got for now 💅</li>
+      </ul>
     </div>
   );
 }
